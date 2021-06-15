27bbb8d80ff82c7f57ec389641cf8b2de92aa950
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2021-06-15 16:14:23 +0300

    fix#3

--- services/sender.js
+++ services/sender.js
@@ -1,50 +1,42 @@
-const Mailgen = require('mailgen');
-const { token } = require('morgan');
+const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
+const configMail = require('../config/config')
 require('dotenv').config()
 
-class EmailServices {
-    constructor(env, sender) {
-        this.sender = sender
-        switch (env) {
-            case 'development':
-                this.link = 'http://localhost:3000'
-                break;
-            case 'production':
-                this.link = 'link from production '
-                break;
-            default:
-                this.link = 'http://localhost:3000'
-                break;
-        }
-    }
-    #createVefifyEmail(tocken, email) {
-        const mailGenerator = new Mailgen({
-            theme: 'salted',
-            product: {
-                name: 'Contacts',
-                link: this.link
-            }
-        })
-    
-        const email = {
-            body: {
-                action: [
-                    {
-                        instructions: 'To get started with Contacts, please click here:',
-                        button: {
-                            color: '#22BC66',
-                            text: 'Confirm your account',
-                            link: `${this.link}/api/contacts/verify/${tocken}`
-                        }
-                    },
-                   
-                ]
-            }
-        };
-        return mailGenerator.generatePlaintext(email)
+class CreateSenderSendgrid {
+    async send(msg){
+     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+            
+       return await sgMail
+          .send({...msg, from: configMail.email.sendgrid})
+
     }
 }
-    
 
-module.exports = EmailServices
+class CreateSenderNodemailer {
+    async send(msg){
+        const config = {
+            host: 'smtp.meta.ua',
+            port: 465,
+            secure: true,
+            auth: {
+              user: 'goitnodejs@meta.ua',
+              pass: process.env.PASSWORD,
+            },
+          }
+          
+          const transporter = nodemailer.createTransport(config)
+          const emailOptions = {
+            from: configMail.email.nodemailer,
+            ...msg,
+          }
+          
+          return await transporter
+            .sendMail(emailOptions)
+    }
+}
 
+module.exports = {
+    CreateSenderSendgrid,
+    CreateSenderNodemailer
+}
